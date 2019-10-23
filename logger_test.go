c82720840819561bf1591fa563d1a9ab2fbc5bea
Sasha Friedenberg <sashafriedenberg@gmail.com>
2019-10-23 11:41:21 -0400

    add more test cases

--- logger_test.go
+++ logger_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/friedenberg/zapstackdriver"
@@ -12,19 +13,22 @@ import (
 )
 
 type jsonObject = map[string]interface{}
-type entry func(l Logger) Logger
-type assertion func(t *testing.T, actual jsonObject)
+type jsonArray = []interface{}
+type entry func(l Logger) []Logger
+type assertion func(t *testing.T, originalLogger Logger, newLoggers []Logger, actual []jsonObject)
 
-func TestLogger(t *testing.T) {
-	for _, testcase := range []struct {
-		description      string
-		serviceContext   zapstackdriver.FieldServiceContext
-		sourceReferences []zapstackdriver.FieldSourceReference
-		entry            entry
-		assertion        assertion
-	}{
+type loggerTestCase struct {
+	description      string
+	serviceContext   zapstackdriver.FieldServiceContext
+	sourceReferences []zapstackdriver.FieldSourceReference
+	entry            entry
+	assertion        assertion
+}
+
+func getLoggerTestCases() []loggerTestCase {
+	return []loggerTestCase{
 		{
-			description: "simple logger",
+			description: "same logger with Error",
 			serviceContext: zapstackdriver.FieldServiceContext{
 				Service: "zapstackdriver",
 				Version: "1.0",
@@ -35,40 +39,102 @@ func TestLogger(t *testing.T) {
 					RevisionId: "some_sha",
 				},
 			},
-			entry: func(l Logger) Logger {
-				l.Errorw("simple")
-				return l
+			entry: func(l Logger) []Logger {
+				l.Error("simple")
+				return []Logger{}
 			},
-			assertion: func(t *testing.T, actual jsonObject) {
+			assertion: func(t *testing.T, originalLogger Logger, newLoggers []Logger, entries []jsonObject) {
+				assert.Len(t, entries, 1)
+				actual := entries[0]
 				assert.Equal(t, "zapstackdriver", actual["serviceContext"].(jsonObject)["service"])
 				assert.Equal(t, "1.0", actual["serviceContext"].(jsonObject)["version"])
-				assert.Equal(t, "type.googleapis.com/google.devtools.clouderrorreporting.v1beta1.ReportedErrorEvent", actual["@type"])
+				assert.Equal(t, zapstackdriver.SDValueErrorType, actual["@type"])
+				assert.Equal(t, "simple", strings.Split(actual["message"].(string), "\n")[0])
+
+				context := actual["context"].(jsonObject)
+				sourceReferences := context["sourceReferences"].(jsonArray)
+				assert.Len(t, sourceReferences, 1)
+				assert.Equal(t, "some_sha", sourceReferences[0].(jsonObject)["revisionId"])
+				assert.Equal(t, "https://github.com/friedenberg/zapstackdriver", sourceReferences[0].(jsonObject)["repository"])
 			},
 		},
-	} {
-		t.Run(
-			testcase.description,
-			func(t *testing.T) {
-				temp, err := ioutil.TempFile("", "zapstackdriver-test-logger")
-				require.NoError(t, err, "Failed to create temp file.")
-				defer os.Remove(temp.Name())
-
-				loggerConfig := zapstackdriver.NewConfig(testcase.serviceContext)
-				loggerConfig.Config.OutputPaths = []string{temp.Name()}
-				logger, err := loggerConfig.Build(testcase.sourceReferences)
-				require.NoError(t, err, "Failed to create logger")
-
-				testcase.entry(logger)
-
-				logged, err := ioutil.ReadFile(temp.Name())
-				require.NoError(t, err, "Failed to read entry from temp file")
-
-				var result jsonObject
-				err = json.Unmarshal(logged, &result)
-				require.NoError(t, err, "Failed to unmarshal entry json")
-
-				testcase.assertion(t, result)
+		{
+			description: "different logger with Error",
+			serviceContext: zapstackdriver.FieldServiceContext{
+				Service: "zapstackdriver",
+				Version: "1.0",
 			},
-		)
+			sourceReferences: []zapstackdriver.FieldSourceReference{
+				zapstackdriver.FieldSourceReference{
+					Repository: "https://github.com/friedenberg/zapstackdriver",
+					RevisionId: "some_sha",
+				},
+			},
+			entry: func(l Logger) []Logger {
+				l = l.With("extra_key", "extra_value")
+				l.Error("simple")
+				return []Logger{l}
+			},
+			assertion: func(t *testing.T, originalLogger Logger, newLoggers []Logger, entries []jsonObject) {
+				assert.Len(t, entries, 1)
+				actual := entries[0]
+				assert.Equal(t, "zapstackdriver", actual["serviceContext"].(jsonObject)["service"])
+				assert.Equal(t, "1.0", actual["serviceContext"].(jsonObject)["version"])
+				assert.Equal(t, zapstackdriver.SDValueErrorType, actual["@type"])
+				assert.Equal(t, "simple", strings.Split(actual["message"].(string), "\n")[0])
+
+				context := actual["context"].(jsonObject)
+				sourceReferences := context["sourceReferences"].(jsonArray)
+				assert.Len(t, sourceReferences, 1)
+				assert.Equal(t, "some_sha", sourceReferences[0].(jsonObject)["revisionId"])
+				assert.Equal(t, "https://github.com/friedenberg/zapstackdriver", sourceReferences[0].(jsonObject)["repository"])
+			},
+		},
+	}
+}
+
+func runLoggerTestCase(t *testing.T, testcase loggerTestCase) {
+	temp, err := ioutil.TempFile("", "zapstackdriver-test-logger.*.json")
+	require.NoError(t, err, "Failed to create temp file.")
+	defer os.Remove(temp.Name())
+
+	loggerConfig := zapstackdriver.NewConfig(testcase.serviceContext)
+	loggerConfig.Config.OutputPaths = []string{temp.Name()}
+	logger, err := loggerConfig.Build(testcase.sourceReferences)
+	require.NoError(t, err, "Failed to create logger")
+
+	loggers := testcase.entry(logger)
+
+	logged, err := ioutil.ReadFile(temp.Name())
+	require.NoError(t, err, "Failed to read entry from temp file")
+
+	lines := strings.Split(string(logged), "\n")
+
+	//drop the last line because logs always append a newline and that results in
+	//an empty string
+	lines = lines[:len(lines)-1]
+
+	logEntries := make([]jsonObject, 0, len(lines))
+
+	for _, line := range lines {
+		var result jsonObject
+		err = json.Unmarshal([]byte(line), &result)
+		require.NoError(t, err, "Failed to unmarshal entry json")
+		logEntries = append(logEntries, result)
+	}
+
+	testcase.assertion(t, logger, loggers, logEntries)
+}
+
+func testCaseRunAdapter(t *testing.T, testcase loggerTestCase, runner func(t *testing.T, testcase loggerTestCase)) func(t *testing.T) {
+	return func(t *testing.T) {
+		t.Parallel()
+		runner(t, testcase)
+	}
+}
+
+func TestLogger(t *testing.T) {
+	for _, testcase := range getLoggerTestCases() {
+		t.Run(testcase.description, testCaseRunAdapter(t, testcase, runLoggerTestCase))
 	}
 }
